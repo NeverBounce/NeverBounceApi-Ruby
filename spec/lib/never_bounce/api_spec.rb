3e3df8ba7481a8bd3be6a68e382930aa526ddb0c
Mike Mollick <mollick2@gmail.com>
2017-10-11 11:22:01 -0400

    Bumping version

--- spec/lib/never_bounce/api_spec.rb
+++ spec/lib/never_bounce/api_spec.rb
@@ -3,6 +3,6 @@ require "never_bounce/api/version"
 
 describe NeverBounce::API do
   describe "VERSION" do
-    it { expect(described_class::VERSION).to eq "1.0.1" }
+    it { expect(described_class::VERSION).to eq "1.0.2" }
   end
 end
