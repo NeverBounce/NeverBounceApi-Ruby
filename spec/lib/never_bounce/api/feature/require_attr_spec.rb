a71fd33cb773166e675279b8b2e27b33cef5fb3a
Erin Paget <erin.dees@hey.com>
2025-08-08 18:05:59 -0400

    Avoid HTTParty deprecation warnings on #nil?

--- spec/lib/never_bounce/api/feature/require_attr_spec.rb
+++ spec/lib/never_bounce/api/feature/require_attr_spec.rb
@@ -21,5 +21,18 @@ module NeverBounce; module API; module Feature
       r.a = "a"
       expect(r.b).to eq ["a", "b"]
     end
+
+    it "doesn't trip HTTParty's deprecation warning on #nil?" do
+      request = instance_double(HTTParty::Request, options: {})
+      response = instance_double(Net::HTTPResponse, body: nil, to_hash: {})
+      parsed_block = lambda { nil }
+
+      server_obj = HTTParty::Response.new(request, response, parsed_block)
+
+      r = klass.new
+      r.a = server_obj
+
+      expect { r.b }.not_to output(/DEPRECATION/).to_stderr
+    end
   end
 end; end; end
